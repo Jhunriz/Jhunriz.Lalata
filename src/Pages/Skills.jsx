07dfeef8ff4f4cac2af117eae86d296bc18544b6
Jhunriz <lalata.jhunriz.bscs2019@gmail.com>
2024-01-27 20:17:16 +0800

    add cards

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -1,19 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import AnimatedSkills from "../Components/AnimatedSkills";
 
-export default function Skills() {
-  const [rotation, setRotation] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setRotation((rotation) => rotation + 1);
-    }, 30);
-
-    return () => clearInterval(intervalId);
-  }, []);
+const Skills = () => {
   return (
-    <div class="grid grid-col lg:grid-cols-4 justify-items-center gap-10 lg:px-96 py-24">
-      <AnimatedSkills title="test" />
+    <div class="grid grid-col w- lg:grid-cols-3 justify-items-center items-center gap-3 py-24">
+      <AnimatedSkills
+        title="HTML5"
+        Description={"sdfsdffsdafsdffasdasdfasdf"}
+        bg={"hover:bg-orange-500"}
+        imageUrl={"https://cdn-icons-png.flaticon.com/512/732/732212.png"}
+      />
+      <AnimatedSkills
+        title="CSS3"
+        Description={"sdfsdffsdafsdffasdasdfasdf"}
+        imageUrl={
+          "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png"
+        }
+      />
+      <AnimatedSkills
+        title="Javascript"
+        Description={"sdfsdffsdafsdffasdasdfasdf"}
+        imageUrl={
+          "https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-transparent-logo-javascript-images-3.png"
+        }
+      />
+      <AnimatedSkills title="PHP" Description={"sdfsdffsdafsdffasdasdfasdf"} />
+      <AnimatedSkills
+        title="MySql"
+        Description={"sdfsdffsdafsdffasdasdfasdf"}
+      />
+      <AnimatedSkills
+        title="TailwindCss"
+        Description={"sdfsdffsdafsdffasdasdfasdf"}
+      />
+      <AnimatedSkills
+        title="Styled-Components"
+        Description={"sdfsdffsdafsdffasdasdfasdf"}
+      />
     </div>
   );
-}
+};
+
+export default Skills;
