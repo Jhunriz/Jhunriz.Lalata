344d1058b72e6331b78b334193d5958f9e87c2d6
Jhunriz <lalata.jhunriz.bscs2019@gmail.com>
2024-04-27 12:12:33 +0800

    changing the portfolio

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -1,117 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import AnimatedNavbarText from "../Components/AnimatedNavbarText";
-import styled from "styled-components";
-import StyledSecondaryButton from "../Components/SecondaryButton";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
-const Logo = styled.a`
-  color: var(--text);
-  text-decoration: none;
-  font-size: 1.5rem;
-  font-weight: bold;
 
-  @media screen and (max-width: 768px) {
-    margin-bottom: 1.5rem;
-  }
-`;
-
-const Nav = styled.nav`
-  background: var(--background);
-  padding: 0 20px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 80px;
-  padding-left: 8rem;
-  padding-right: 8rem;
-  position: relative;
-  z-index: 10;
-  border-bottom: 2px solid var(--accent);
-
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-  }
-`;
-
-const MenuIcon = styled.div`
-  display: none;
-  color: #160df9;
-  font-size: 1.5rem;
-  cursor: pointer;
-
-  @media screen and (max-width: 768px) {
-    display: block;
-  }
-`;
-
-const Menu = styled.div`
-  display: flex;
-  align-items: center;
-
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-    position: absolute;
-    top: 80px;
-    left: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
-    width: 100%;
-    background: #333;
-    transition: left 0.3s ease-in-out;
-  }
-`;
-
-const MenuItem = styled.a`
-  color: var(--text);
-  text-decoration: none;
-  font-size: 1.2rem;
-  margin-right: 20px;
-
-  &:hover {
-    color: var(--text);
-  }
-
-  @media screen and (max-width: 768px) {
-    margin: 10px 0;
-  }
-`;
-
-const StyledNavbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
-
-  useEffect(() => {
-    AOS.init();
-  }, []);
+export default function Navbar() {
   return (
     <div>
-      <Nav>
-        <Logo href="#" data-aos="fade-right">
-          DEV.J
-        </Logo>
-        <MenuIcon onClick={toggle}>
-          <i className={isOpen ? "X" : "X"}></i>
-        </MenuIcon>
-        <Menu isOpen={isOpen} data-aos="fade-right">
-          <MenuItem>
-            <Link to={"/"}>Home</Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to={"/Skills"}>Skills</Link>
-          </MenuItem>
-          <MenuItem href="#">Services</MenuItem>
-          <MenuItem href="#">Contact</MenuItem>
-          <MenuItem href="#">
-            <StyledSecondaryButton title="DOWNLOAD CV">
-              Download CV
-            </StyledSecondaryButton>
-          </MenuItem>
-        </Menu>
-      </Nav>
+      <nav className='p-5'>
+        <ul className='flex flex-grow justify-between items-center'>
+          <li className='text-white'>logo</li>
+          <div className='flex flex-grow justify-end items-center gap-5'>
+          <li className='text-white'>Home</li>
+          <li className='text-white'>Portfolio</li>
+          <li className='text-white'>CV</li>
+          <li><button className='bg-blue-400 p-2 rounded shadow-lg shadow-blue-700'>DOWNLOAD</button></li>
+          </div>
+        </ul>
+      </nav>
     </div>
-  );
-};
-
-export default StyledNavbar;
+  )
+}
